dd818bcd414da6fc14145554289f70542ccd957d
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-05-31 14:50:08 -0400

    Pushing recent changes to production

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://production.dcs2jw2334qwo.amplifyapp.com/',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
