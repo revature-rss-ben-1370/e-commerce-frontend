810c4b2588b477dc4b639bf70a3eb94afb28be42
tsimmons15 <tsimmons15@knights.ucf.edu>
2022-05-31 15:05:16 -0400

    Search Button CSS tweaks (#29) (#32)
    * Added backend host to cCommerceClient.ts
    
    * Dealing with CORS
    
    * Dealing with CORS
    
    * Dealing with CORS
    
    * Update eCommerceClient.ts
    
    * Initialized frontend and backend hosting.
    
    * Pulling in recently pushed changes
    
    * Update eCommerceClient.ts
    
    Switching over to an S3 backed EBS instance.
    
    * switching away from EBS instance.
    
    * Added search button tied to search bar.
    
    * Send to live. (#28)
    
    * Validate first and last names
    
    * basic css edits (#26)
    
    * basic css edits
    
    * Delete package-lock.json
    
    * Add files via upload
    
    * Sales update (#25)
    
    * Update to the sales feature
    
    * Made updates to the sales price feature to not include sales Flat
    
    * This is an update of the Sales frontend to display in both the checkout cart and the payment info
    
    * Product detail view quantity select (#24)
    
    * Add to cart function
    
    Added the ability to add to cart from the Product Detail View. Also gives an message when an item is added to the cart. Currently this message displays regardless of success or failure.
    
    * Moved Snackbar to ProductCard
    
    Moved the snack bar message to the Product Card class so that it shows only when necessary.
    
    * Added search button tied to search bar. (#23)
    
    Co-authored-by: tsimmons15 <tsimmons15@valenciacollege.edu>
    
    * Add increment/decrement buttons to the cart input (#22)
    
    Co-authored-by: jalil <jalil.ahmed.harmony@gmail.com>
    Co-authored-by: dmbrumagin <83185754+dmbrumagin@users.noreply.github.com>
    Co-authored-by: bmokua <brianmmokua@gmail.com>
    Co-authored-by: Grayson Howard <mhoward@mtu.edu>
    Co-authored-by: tsimmons15 <tsimmons15@valenciacollege.edu>
    Co-authored-by: Zanetta Tyler <zrtyler@aggies.ncat.edu>
    
    * Fixed the CSS for Light Mode by tweaking the properties used.
    
    * Reverted baseURL pulled from production.
    
    Co-authored-by: tsimmons15 <tsimmons15@valenciacollege.edu>
    Co-authored-by: jalil <jalil.ahmed.harmony@gmail.com>
    Co-authored-by: dmbrumagin <83185754+dmbrumagin@users.noreply.github.com>
    Co-authored-by: bmokua <brianmmokua@gmail.com>
    Co-authored-by: Grayson Howard <mhoward@mtu.edu>
    Co-authored-by: Zanetta Tyler <zrtyler@aggies.ncat.edu>
    
    Co-authored-by: tsimmons15 <tsimmons15@valenciacollege.edu>
    Co-authored-by: jalil <jalil.ahmed.harmony@gmail.com>
    Co-authored-by: dmbrumagin <83185754+dmbrumagin@users.noreply.github.com>
    Co-authored-by: bmokua <brianmmokua@gmail.com>
    Co-authored-by: Grayson Howard <mhoward@mtu.edu>
    Co-authored-by: Zanetta Tyler <zrtyler@aggies.ncat.edu>

--- src/App.tsx
+++ src/App.tsx
@@ -16,37 +16,37 @@ function App() {
   //** EDIT START: Dark Mode Switcher **//
   const lightMode = createTheme({
     palette: {
-      primary: {main: '#474C55', contrastText: "#fff"},
-      secondary: {main:'#bbbbbb', contrastText: "#fff"},
-      text: {primary: '#000000'},
-      background: {default: '#ffffff'}
+      primary: { main: '#474C55', contrastText: "#000" },
+      secondary: { main: '#bbbbbb', contrastText: "#000" },
+      text: { primary: '#000000' },
+      background: { default: '#ffffff' }
     }
   });
 
   const darkMode = createTheme({
     palette: {
-      primary: {main: '#B9B9BA', contrastText: "#fff"},
-      secondary: {main:'#B9B9BA', contrastText: "#fff"},
-      text: {primary: '#ffffff', secondary: '#ffffff'},
-      background: {default: '#212121', paper: '#212121'}
+      primary: { main: '#B9B9BA', contrastText: "#fff" },
+      secondary: { main: '#B9B9BA', contrastText: "#fff" },
+      text: { primary: '#ffffff', secondary: '#ffffff' },
+      background: { default: '#212121', paper: '#212121' }
     }
   });
 
   const colorMode = sessionStorage.getItem('colorMode');
   let theme = colorMode != 'lightMode' ? darkMode : lightMode;
- //** EDIT END: Dark Mode Switcher (also adds ThemeProvider, GlobalStyles, and empty tags to the return, but I can't comment inside there.) **//
+  //** EDIT END: Dark Mode Switcher (also adds ThemeProvider, GlobalStyles, and empty tags to the return, but I can't comment inside there.) **//
 
   return (
     <ThemeProvider theme={theme}>
       <Typography color="textPrimary">
-        <GlobalStyles styles ={{ body:{backgroundColor:theme.palette.background.default}}} />
+        <GlobalStyles styles={{ body: { backgroundColor: theme.palette.background.default } }} />
         <>
-        <DarkModeSwitcher />
-        <CartContext.Provider value={value}>
-          <Router>
-            <AppRoutes></AppRoutes>
-          </Router>
-        </CartContext.Provider>
+          <DarkModeSwitcher />
+          <CartContext.Provider value={value}>
+            <Router>
+              <AppRoutes></AppRoutes>
+            </Router>
+          </CartContext.Provider>
         </>
       </Typography>
     </ThemeProvider>
