0037f41afd77a6e3f1a0091b98e06618a1bb6ca5
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-05-31 14:53:29 -0400

    Fixed issue with eCommerceClient

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -4,11 +4,7 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
   withCredentials: true,
-<<<<<<< HEAD
-  baseURL: 'https://production.dcs2jw2334qwo.amplifyapp.com/',
-=======
   baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
->>>>>>> 0b8f719c1b299744499ba27dde71972bcda3cf8d
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
