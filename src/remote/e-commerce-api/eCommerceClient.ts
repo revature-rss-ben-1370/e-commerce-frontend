ef9bf2ba97b443730e36a56229be7811a83da363
ASchro1 <aaron243@revature.com>
2022-06-30 17:12:04 -0400

    url changed

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
+  baseURL: 'http://back-end-service.p3-space.svc.cluster.local:5000',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
